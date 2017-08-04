91dce95ffaebbef59060641fed03a69aa4db05f4
sanjana <sanjana@gemsessence.com>
2017-08-04 13:29:21 +0530

    send-email-to-gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,20 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   #added host & port
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    address:              'smtp.gmail.com',
+    port:                 587,
+    # :tls                  => true,
+    domain:               'gmail.com',  
+    authentication:       'plain',
+    user_name:            'sanjana@gemsessence.com',
+    password:             '9039616166' 
+  }
+
+
 end
